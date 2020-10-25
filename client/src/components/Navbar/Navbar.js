e38b20ad6d9d63ff724f89ca0c897c20220c5747
ShaneMit <shanemit@cox.net>
2020-10-25 16:57:02 -0700

    Fixed navbar link

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">Shane Mitchell</a>
+        <a className="navbar-brand" href="/">Shane Mitchell</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
